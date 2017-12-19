7380dedb1315b36c4de0c1cba2a8dfdf35386ff9
Sofyan Thayf <sofyanthayf@gmail.com>
2017-12-19 14:25:43 +0800

    First Commit

---
+++ application/views/undercons.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>K-Innovation Labs</title>
+  </head>
+  <body style="text-align:center;">
+    <img src="/assets/img/website-under-construction.png" alt="">
+  </body>
+</html>
